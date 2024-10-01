12f73a9ea8aaa35db62596b9d4395a82a4518903
unknown <augustodhoff@gmail.com>
2024-10-01 00:22:15 -0300

    teste 3

--- src/Teste1/Main.java
+++ src/Teste1/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("TESTESÃO e vai tudo p krl");
+		System.out.println("mto loko isso codigo na web");
 	}
 
 }
